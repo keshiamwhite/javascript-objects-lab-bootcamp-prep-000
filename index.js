52125063c5da62d260e07eb516abde1dbc080aad
Keshia White <keshiamwhite@gmail.com>
2018-08-23 22:43:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var recipes = {
-  
+  prop: 1,
+  prop2: []
 };
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  object {[prop] = [1], prop2 = [2]}
+function updateObjectWithKeyAndValue(recipes, prop2, value) {
+  object{[prop] = [1], prop2 = [2]}
   return object;
 }
